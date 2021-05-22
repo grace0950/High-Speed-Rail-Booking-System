aaf9fd3d5f002823c5c55e5e263792e42c297c4f
gf0927 <60601benson@gmail.com>
2021-05-23 01:04:21 +0800

    add topic-creation

---
+++ kafka_backend/Topic-Creation.sh
@@ -0,0 +1,8 @@
+#This is the shell file that create the kafka topic
+
+#first, given where you install kafka, there should be a bin folder include zookeeper-server-start.sh, kafka-server-start.sh
+#And given two config file in the config folder..
+cd "/home/gf0927/kafka/kafka_2.13-2.8.0"
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic login_topic
