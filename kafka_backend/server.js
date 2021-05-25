1b4902e80ff069ba6da9d2659d9c00ebb4dae453
grace0950 <pinyan0950@gmail.com>
2021-05-25 22:20:08 +0800

    update kafka backend

--- kafka_backend/server.js
+++ kafka_backend/server.js
@@ -1,41 +1,38 @@
-let connection =  new require('./Connection');
+let connection = new require("./Connection");
 
 let producer = connection.getProducer();
 
-
 let loginConsumer = connection.getConsumerObj("login_topic");
 
+let login = require("./services/login");
 
-try{
-    loginConsumer.on('message', function (message) {
-        console.log('message received');
-        console.log(JSON.stringify(message.value));
-        let data = JSON.parse(message.value);
+try {
+  loginConsumer.on("message", function (message) {
+    console.log("message received");
+    console.log(JSON.stringify(message.value));
+    let data = JSON.parse(message.value);
 
-        console.log(data.replyTo);
+    console.log(data);
 
-        login.handle_request(data.data, function (err, res) {
-            console.log('after handle' + res);
-            let payloads = [
-                {
-                    topic: data.replyTo,
-                    messages: JSON.stringify({
-                        correlationId: data.correlationId,
-                        data: res
-                    }),
-                    partition: 0
-                }
-            ];
-            producer.send(payloads, function (err, data) {
-                // console.log(data);
-                console.log(payloads);
-            });
-            // return;
-            
-        });
+    login.handle_request(data, function (err, res) {
+      console.log("after handle" + res);
+      let payloads = [
+        {
+          topic: data.replyTo,
+          messages: JSON.stringify({
+            correlationId: data.correlationId,
+            data: res,
+          }),
+          partition: 0,
+        },
+      ];
+      producer.send(payloads, function (err, data) {
+        // console.log(data);
+        console.log(payloads);
+      });
+      // return;
     });
-}
-
-catch (e) {
-    console.log(e)
+  });
+} catch (e) {
+  console.log(e);
 }
