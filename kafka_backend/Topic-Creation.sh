1f819807760688c034cab4d58e2f9d4b1e193861
grace0950 <pinyan0950@gmail.com>
2021-06-01 19:07:14 +0800

    script

--- kafka_backend/Topic-Creation.sh
+++ kafka_backend/Topic-Creation.sh
@@ -2,9 +2,12 @@
 
 #first, given where you install kafka, there should be a bin folder include zookeeper-server-start.sh, kafka-server-start.sh
 #And given two config file in the config folder..
-cd "/home/gf0927/kafka/kafka_2.13-2.8.0"
+cd "~/Kafka/kafka_2.13-2.8.0"
 bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/kafka-server-start.sh config/server.properties
 
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic login_topic
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic response_topic
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic signup_topic
 
 echo "Topic Create Success!"
